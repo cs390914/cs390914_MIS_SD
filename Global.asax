fd7fe1930a5f89745a2bfa0ff79a629c9ed338e3
Shannon <cs390914@ohio.edu>
2019-10-30 17:41:43 -0400

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cs390914_MIS_SD.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cs390914_MIS_SD.Startup))]
+namespace cs390914_MIS_SD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
