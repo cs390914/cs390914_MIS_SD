dce39f16bf6c52ed02500159c5f31de140441ee3
Clay Shannon <cs390914@ohio.edu>
2019-10-30 21:44:07 -0400

    beginning of SD2

---
+++ Models/Doctors.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace cs390914_MIS_SD.Models
+{
+    public class Doctors
+    {
+        public int DoctorID { get; set; }
+        public string AreaOfPractice { get; set; }
+        public DateTime DoctorSince { get; set; }
+        public string DoctorPhone { get; set; }
+
+        public ICollection<Visits> Visits { get; set; }
+    }
+}
---
+++ Models/Visits.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace cs390914_MIS_SD.Models
+{
+    public class Visits
+    {
+        public int VisitID { get; set; }
+        public string ReasonForVisit { get; set; }
+        public DateTime VisitDate { get; set; }
+
+        public int PatientID { get; set; }
+        public virtual Patient Patient { get; set; }
+    }
+}
