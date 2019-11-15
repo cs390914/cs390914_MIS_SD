63092232cdf78d54f1515366238d61d71141e0d5
Clay Shannon <cs390914@ohio.edu>
2019-11-15 11:39:01 -0500

    controllers added

---
+++ Controllers/DoctorsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using cs390914_MIS_SD.DAL;
+using cs390914_MIS_SD.Models;
+
+namespace cs390914_MIS_SD.Controllers
+{
+    public class DoctorsController : Controller
+    {
+        private MIS4200Context db = new MIS4200Context();
+
+        // GET: Doctors
+        public ActionResult Index()
+        {
+            return View(db.Doctors.ToList());
+        }
+
+        // GET: Doctors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Doctors doctors = db.Doctors.Find(id);
+            if (doctors == null)
+            {
+                return HttpNotFound();
+            }
+            return View(doctors);
+        }
+
+        // GET: Doctors/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Doctors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DoctorID,AreaOfPractice,DoctorSince,DoctorPhone")] Doctors doctors)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Doctors.Add(doctors);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(doctors);
+        }
+
+        // GET: Doctors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Doctors doctors = db.Doctors.Find(id);
+            if (doctors == null)
+            {
+                return HttpNotFound();
+            }
+            return View(doctors);
+        }
+
+        // POST: Doctors/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "DoctorID,AreaOfPractice,DoctorSince,DoctorPhone")] Doctors doctors)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(doctors).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(doctors);
+        }
+
+        // GET: Doctors/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Doctors doctors = db.Doctors.Find(id);
+            if (doctors == null)
+            {
+                return HttpNotFound();
+            }
+            return View(doctors);
+        }
+
+        // POST: Doctors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Doctors doctors = db.Doctors.Find(id);
+            db.Doctors.Remove(doctors);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
